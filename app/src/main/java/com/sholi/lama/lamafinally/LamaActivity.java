816fb9607ef5fc832d980f80a9f21a8b2f1ddfd4
user <muhammed.awad@outlook.com>
2018-10-10 08:39:23 +0300

    Initial commit

---
+++ app/src/main/java/com/sholi/lama/lamafinally/LamaActivity.java
@@ -0,0 +1,13 @@
+package com.sholi.lama.lamafinally;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LamaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lama);
+    }
+}
